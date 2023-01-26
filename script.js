ed57f931188d028a2f9eeeecd65b55a51aa707f4
yomunnib <muneeb.cheema36@gmail.com>
2023-01-25 23:38:43 -0500

    Finish Modal.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let myLibrary = [];
  const newAuthor= document.getElementById("new-author");
  const newPages= document.getElementById("new-pages");
  const newRead= document.getElementById("new-read");
+ const modal = document.getElementById("modal");
+ const addButton = document.getElementById("add");
 
 class Book {
     constructor(title, author, pages, read) {
@@ -22,6 +24,15 @@ function submitForm() {
     myLibrary.push(new Book(newTitle.value,newAuthor.value,newPages.value,newRead.value));
   }
 
+  addButton.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  })
  
  
 
