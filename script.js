1a2eabc07b96cacd7ebf101d76868a8363c3165a
yomunnib <muneeb.cheema36@gmail.com>
2023-01-26 07:34:55 -0500

    Add book object prototype and finish form submittal js. Work on create/delete book function.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let myLibrary = [];
- const newTitle= document.getElementById("new-title");
- const newAuthor= document.getElementById("new-author");
- const newPages= document.getElementById("new-pages");
- const newRead= document.getElementById("new-read");
- const modal = document.getElementById("modal");
- const addButton = document.getElementById("add");
+let saved = [];
+const modal = document.getElementById("modal");
+const addButton = document.getElementById("add");
+const submitForm = document.getElementById("submit-form");
+const bookShelf = document.getElementById("book-shelf");
+const closeButton = document.getElementsByClassName("close")[0];
+const form = document.querySelector("form");
 
 class Book {
     constructor(title, author, pages, read) {
@@ -15,26 +16,54 @@ class Book {
     }
 }
 
+myLibrary.prototype = Object.create(Book.prototype);
 
-    function addBookToLibrary() {
-        
-      }
+function addBookToLibrary() {
+    myLibrary.push(new Book(saved['title'],saved['author'],saved['pages'],saved['read']));
+}
 
-function submitForm() {
-    myLibrary.push(new Book(newTitle.value,newAuthor.value,newPages.value,newRead.value));
+function deleteBook(key) {
+    myLibrary.filter(val => !val.hasOwnProperty[key]);
   }
+
+function createBook(key) {
+  const book = document.createElement("div");
+  const bookChild = book.appendChild(book);
+  bookShelf.appendChild(book).className = "book";
+  book.appendChild(book).className = "book-text";
+  bookText.appendChild(book).className = "name";
+  bookText.appendChild(book).className = "author";
+  bookText.appendChild(book).className = "pages";
+  elementInsideDiv.insertAdjacentHTML('afterbegin', myLibrary[key].title);
+
+
+}
+
+ 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const bookForm = new FormData(form);
+
+    bookForm.forEach((value,key) => (saved[key] = value));
+    console.log(saved);
+    modal.style.display = "none";
+    form.reset();
+    addBookToLibrary();
+});
 
   addButton.onclick = function() {
     modal.style.display = "block";
   }
 
-  window.addEventListener("click", function(event) {
-    if (event.target == modal) {
+  closeButton.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  window.addEventListener("mousedown", (e) => {
+    if (e.target === modal) {
         modal.style.display = "none";
     }
   })
- 
- 
-
 
 
