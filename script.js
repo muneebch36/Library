afb6c116ff7277c941010cedcee206afb992f3aa
yomunnib <muneeb.cheema36@gmail.com>
2023-01-30 02:11:52 -0500

    Finish project.

--- script.js
+++ script.js
@@ -7,15 +7,26 @@ const bookShelf = document.getElementById("book-shelf");
 const closeButton = document.getElementsByClassName("close")[0];
 const form = document.querySelector("form");
 const deleteButton = document.querySelectorAll(".material-symbols-outlined");
+const defaultToggle = document.getElementById("no-longer");
 let newBooks = ""; //stores dynamically generated book nodelist so that it can be accessed in a function outside of the function it was updated in.
 
+
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
+  constructor(title, author, pages, read, like) {
+      this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
+        this.like = like;
     }
+}
+
+defaultToggle.onclick = function () { //defualt book toggle function to avoid myLibrary object errors
+  if (defaultToggle.innerText.includes(")")) {
+    defaultToggle.innerText = "Me No Like :("
+  } else {
+    defaultToggle.innerText = "I Like :)"
+  }
 }
 
 addButton.onclick = function() { //modal display + button
@@ -38,7 +49,11 @@ form.addEventListener("submit", (e) => {
   const bookForm = new FormData(form);
 
   bookForm.forEach((value,key) => (saved[key] = value)); //save form data into array
-  console.log(saved);
+  
+  if (saved.like === "on") { //change default values of checkbox
+    saved.like = "I Like :)"
+  } else saved.like = "Me No Like :("
+
   modal.style.display = "none";
   form.reset();
   addBookToLibrary(); 
@@ -47,7 +62,7 @@ form.addEventListener("submit", (e) => {
 myLibrary.prototype = Object.create(Book.prototype);
 
 function addBookToLibrary() { //push saved form data array into myLibrary object
-    myLibrary.push(new Book(saved.title,saved.author,saved.pages,saved.read));
+    myLibrary.push(new Book(saved.title,saved.author,saved.pages,saved.read,saved.like));
     placeBook();
 }
 
@@ -64,18 +79,23 @@ function resetShelf () { //replace books in bookshelf to prevent duplicates as p
   });
 }
 
+document.addEventListener("DOMNodeInserted", function(){ //keeps an updated array of dynamically genereated books so that they can be targeted separately for deletion than default book.
+  newBooks = document.querySelectorAll(".new-book");
+});
+
 function createBook(index) { //creates HTML content for books that need to be placed.
   
   bookShelf.insertAdjacentHTML('afterbegin', `
   <div class="new-book" id=${index}>
+    <div class="book-cover">
+      <div class="name">${myLibrary[index].title}</div>
+      <div class="author">${myLibrary[index].author}</div>
+      <div class="pages">${myLibrary[index].pages} pgs</div>
+    </div>
   <img src="book.png" alt="book"/>
-  <div class="book-text">
-    <div class="name">${myLibrary[index].title}</div>
-    <div class="author">${myLibrary[index].author}</div>
-    <div class="pages">${myLibrary[index].pages} pgs</div>
-  </div>
   <div class="book-status">
     <span class="read">${readPercentage(index)}%</span>
+    <button class="toggle">${myLibrary[index].like}</button>
     <span class="material-symbols-outlined">Delete</span>
   </div>
 </div>`
@@ -87,14 +107,11 @@ function readPercentage(index) {
   return Math.round(x);
 } 
 
-document.addEventListener("DOMNodeInserted", function(){ //keeps an updated array of dynamically genereated books so that they can be targeted separately for deletion than default book.
-  newBooks = document.querySelectorAll(".new-book");
-});
-
 document.addEventListener("click", function(e){ //keeps an updated array of dynamically generated delete icons.
-  
-    if(target = (e.target.closest(".material-symbols-outlined")) ){
-      const burnBook = (e.target.closest('.new-book'));
+  const burnBook = (e.target.closest('.new-book'));
+  const toggleButton = (e.target.closest(".toggle"));
+
+  if(target = (e.target.closest(".material-symbols-outlined")) ){
         if (burnBook === null) { //delete default book
           (e.target.closest('.book')).remove();
         } else { //delete dynamically generated book
@@ -102,10 +119,16 @@ document.addEventListener("click", function(e){ //keeps an updated array of dyna
           burnBook.remove();
           placeBook();
         }  
-    } else if (target = (e.target.closest(".read"))) {
-          alert("edit function placeholder");
-    }
-  });
+} else if (target = (e.target.closest(".toggle")) ){
+        if (toggleButton.innerText.includes(")")) {
+          toggleButton.innerText = "Me No Like :("
+          myLibrary[(burnBook.id)].like = "Me No Like :("
+        } else {
+          toggleButton.innerText = "I Like :)"
+          myLibrary[(burnBook.id)].like = "I Like :)"
+        }
+}
+    });
   
   /*   if (checkDuplicate()) {
     alert("book exists");
